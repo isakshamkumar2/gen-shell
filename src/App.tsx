0c363cb17b85948da714d2879cedf365dcffff46
saksham <saksham@genflowly.com>
2024-04-30 14:56:16 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from './Landing';
+import CarPage from './CarPage';
 
-type Props = {};
-
-const App = (props: Props) => {
-  return <h2>hi</h2>;
-};
+function App() {
+  return (
+    <BrowserRouter basename="/subapp-2">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/cars/:id" element={<CarPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-let HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const webpack = require('webpack');
 const deps = require('./package.json').dependencies;
-console.log(process.env.REACT_APP_VAR);
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: 'production',
+  entry: path.resolve(__dirname, './src/index.tsx'),
   output: {
-    publicPath: 'http://localhost:3000/',
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
   },
   devServer: {
-    port: 3000,
     historyApiFallback: {
-      index: 'index.html',
+      index: '/index.html',
     },
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
   },
+
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+    new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.ProgressPlugin({
       activeModules: true,
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
     new ModuleFederationPlugin({
-      name: 'HOST',
+      name: 'Shell',
       filename: 'remoteEntry.js',
       exposes: {
         './Shell': './src/App.tsx',
